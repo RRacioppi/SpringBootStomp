c8df776d704711975ed6789c68f42a758fcf0516
Renzo Racioppi <rracioppi@imolainformatica.it>
2020-12-06 16:09:48 +0100

    initial commit

---
+++ src/test/java/com/example/demp/websocketstomp/WebsocketstompApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demp.websocketstomp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketstompApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
